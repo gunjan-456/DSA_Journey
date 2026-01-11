f701dcb67d6c19af98ff9acdb8b1e5671fc3136c
Gunjan Singh <gunjansingh72002@gmail.com>
2026-01-11 23:50:47 +0530

    commit

---
+++ intersectionArray/app.js
@@ -0,0 +1,15 @@
+var intersection = function(nums1, nums2) {
+    let set1 = new Set(nums1)     
+    let ansSet = new Set()   
+
+    for (let num of nums2) {
+        if (set1.has(num)) {
+            ansSet.add(num)
+        }
+    }
+
+    return Array.from(ansSet)
+
+    
+}
+console.log(intersection([1, 2, 2, 1], [2, 2]))
