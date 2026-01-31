90630d824dfd9bccc6d91bca5026dc87e77f502b
Gunjan Singh <gunjansingh72002@gmail.com>
2026-02-01 00:01:51 +0530

    commit

---
+++ Array/Basic/number_evenNumberDigits.js
@@ -0,0 +1,18 @@
+//Question : 1295
+
+
+
+function findNumbers(nums){
+    let count = 0
+    for(let i = 0; i < nums.length; i++)
+    {
+        if (nums[i].toString().length % 2 === 0) {
+            count++
+        }
+    }
+    return count
+}
+console.log(findNumbers([12,345,2,6,7896]))  //2
+console.log(findNumbers([555,901,482,1771]))  //1
+
+//Time complexity :O(n)
