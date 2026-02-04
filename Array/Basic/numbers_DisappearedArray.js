212cf6f15bdc7afd07c24800b57f69e1cfd7ee3f
Gunjan Singh <gunjansingh72002@gmail.com>
2026-02-04 23:59:22 +0530

    commit

--- Array/Basic/numbers_DisappearedArray.js
+++ Array/Basic/numbers_DisappearedArray.js
@@ -22,20 +22,21 @@
 
 
 function findDisappearedNumbers(nums) {
+    
+    let n = nums.length
+    for (let i = 0; i < n; i++) {
+        let val = nums[i]
+        let idx = (val - 1) % n
+        nums[idx] += n
+    }
+    
+    let ans = []
+    for (let i = 0; i < n; i++) {
+        if (nums[i] <= n) ans.push(i + 1)
+    }
 
-
-  let n = nums.length
-  for (let i = 0; i < n; i++) {
-    let val = nums[i];
-    let idx = (val - 1) % n
-    nums[idx] += n;
-  }
-
-  let ans = [];
-  for (let i = 0; i < n; i++) {
-    if (nums[i] <= n) ans.push(i + 1);
-  }
-
-  return ans;
+    return ans;
 }
-console.log(findDisappearedNumbers([1, 2, 3, 4, 7, 8]))
+console.log(findDisappearedNumbers([1, 2, 3, 4, 7, 8]))      //[5, 6]
+console.log(findDisappearedNumbers([1, 1]))                 // [2]     //Time Complexity : O(n)
+                                                                       //Space complexity : O(1)
