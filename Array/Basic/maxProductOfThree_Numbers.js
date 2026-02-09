64cfcd8334909e0cebfec1053e9b307a01f669dc
Gunjan Singh <gunjansingh72002@gmail.com>
2026-02-10 02:09:37 +0530

    commit

--- Array/Basic/maxProductOfThree_Numbers.js
+++ Array/Basic/maxProductOfThree_Numbers.js
@@ -1,2 +1,17 @@
 //Leetcode : 628
 
+function maximumProduct(nums)
+{
+    nums.sort((a, b) => a - b)
+
+    let n = nums.length
+
+    let case1 = nums[n-1] * nums[n-2] * nums[n-3]
+    let case2 = nums[0] * nums[1] * nums[n-1]
+
+    return Math.max(case1, case2)
+}
+console.log(maximumProduct([1, 2, 3]))          //6
+console.log(maximumProduct([1, 2, 3, 4]))       //24
+console.log(maximumProduct([1, 2, 3, 4, 5]))    //60                        Time Complexity : O(n logn)
+                                                                            //Space Complexity : O(1)
