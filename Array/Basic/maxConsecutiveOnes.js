9fc0968a3195cec8afeeefbd557c1b488930c5fe
Gunjan Singh <gunjansingh72002@gmail.com>
2026-02-10 00:56:35 +0530

    commit

--- Array/Basic/maxConsecutiveOnes.js
+++ Array/Basic/maxConsecutiveOnes.js
@@ -1,1 +1,24 @@
 //Leetcode : 485
+
+
+function maxConsecutiveOnes(nums){
+    let count = 0
+    let maxCount = 0
+    for(let i = 0; i < nums.length; i++)
+    {
+        if(nums[i] === 1)
+        {
+            count++
+            maxCount = Math.max(maxCount, count)
+
+        }
+        else{
+            count = 0
+        }
+    }
+    return maxCount
+
+}
+console.log(maxConsecutiveOnes([1,1,0,1,1,1]))       //3
+console.log(maxConsecutiveOnes([1,0,1,1,0,1]))       //2                      Time Complexity : O(n)
+                                                                              //Space Complexity : O(1)
