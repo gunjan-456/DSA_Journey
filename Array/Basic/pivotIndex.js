6418c087b9d0464414488d22bc4c06fa1bd548b0
Gunjan Singh <gunjansingh72002@gmail.com>
2026-02-12 00:26:56 +0530

    commit

--- Array/Basic/pivotIndex.js
+++ Array/Basic/pivotIndex.js
@@ -2,5 +2,28 @@
 
 function pivotIndex(nums)
 {
-    
+
+    let totalSum = 0
+    for(let x of nums)
+    {
+        totalSum += x
+    }
+
+    let leftSum = 0
+    for(let i = 0; i < nums.length; i++)
+    {
+        let rightSum = totalSum  - leftSum - nums[i]
+
+        if(leftSum === rightSum)
+        {
+            return i
+        }
+        leftSum += nums[i]
+    }
+    return -1
+
 }
+console.log(pivotIndex([1,7,3,6,5,6]))        //3
+console.log(pivotIndex([1,2,3]))              //-1
+console.log(pivotIndex([2,1,-1]))            //0                       //Time Complexity : O(n)
+                                                                       //Space Complexity : O(1)
