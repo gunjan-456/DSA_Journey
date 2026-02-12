10c280e11c693a80184b1db36740d2e2c67fc00b
Gunjan Singh <gunjansingh72002@gmail.com>
2026-02-13 00:03:59 +0530

    commit

--- Array/Basic/sortArrayByParity.js
+++ Array/Basic/sortArrayByParity.js
@@ -3,5 +3,17 @@
 
 function sortArrayByParity(nums)
 {
-    
+    let i = 0
+    for(let j = 0; j < nums.length; j++)
+    {
+        if(nums[j] % 2 === 0)
+        {
+            [nums[i], nums[j]] = [nums[j], nums[i]]
+            i++
+        }
+    }
+    return nums
 }
+console.log(sortArrayByParity([3,1,2,4]))
+console.log(sortArrayByParity([3,1,2,4,6,8,9]))                 //Time Complexity : O(n)
+                                                                //Space Complexity : O(1)
