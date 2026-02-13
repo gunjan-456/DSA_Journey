74ad4522f8851b15d7096a06982188ba112e8bdd
Gunjan Singh <gunjansingh72002@gmail.com>
2026-02-14 00:14:12 +0530

    commit

--- Array/Basic/arrayPartition.js
+++ Array/Basic/arrayPartition.js
@@ -2,5 +2,20 @@
 
 function arrayPartition(nums)
 {
-    
+    nums.sort((a, b) => a - b)
+
+    let sum = 0
+
+    for(let i = 0; i <  nums.length; i+=2)
+    {
+        let a = nums[i]
+        let b = nums[i+1]
+        sum += Math.min(a, b)
+
+    }
+    return sum
+
 }
+console.log(arrayPartition([1, 4, 3, 2]))           //4
+console.log(arrayPartition([6,2,6,5,1,2]))          //9             Time Complexity : O(Nlogn)
+                                                                   //Space Complexity : O(n)
