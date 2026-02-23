9fb36b7dc353bb69e3d700e3d31a2a3048a0ef2f
Gunjan Singh <gunjansingh72002@gmail.com>
2026-02-24 01:00:52 +0530

    commit

--- Array/Basic/distanceValues.js
+++ Array/Basic/distanceValues.js
@@ -1,1 +1,28 @@
 //Leetcode : 1385
+
+
+
+function findTheDistanceValue(arr1, arr2, d) {
+    let count = 0;
+
+    for (let i = 0; i < arr1.length; i++) {
+        let isValid = true;
+
+        for (let j = 0; j < arr2.length; j++) {
+            if (Math.abs(arr1[i] - arr2[j]) <= d) {
+                isValid = false;
+                break;
+            }
+        }
+
+        if (isValid) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+console.log(findTheDistanceValue([4,5,8], [10,9,1,8], 2))     //2
+console.log(findTheDistanceValue([1,4,2,3], [-4,-3,6,10,20,30], 3))         //2             Time Complexity : O(n*m)
+                                                                                        //Space Complexity : O(1)
