1eca6d5d85381467dd9e15d1926ff83dfcdae38d
Gunjan Singh <gunjansingh72002@gmail.com>
2026-02-24 01:22:50 +0530

    commit

--- Array/Basic/maximumCount_positiveNegative.js
+++ Array/Basic/maximumCount_positiveNegative.js
@@ -2,5 +2,24 @@
 
 function maximumCount(nums)
 {
+    let positiveCount = 0
+    let negativeCount = 0
+
+    for(let item of nums)
+    {
+        if(item < 0)
+        {
+            negativeCount++
+        }
+        if(item > 0)
+        {
+            positiveCount++
+        }
+    }
+    return Math.max(positiveCount, negativeCount)
     
 }
+console.log(maximumCount([-2,-1,-1,1,2,3]))                //3
+console.log(maximumCount([-3,-2,-1,0,0,1,2]))              //3
+console.log(maximumCount([5,20,66,1314]))                  //4                      //Time Complexity : O(n)
+                                                                                   //Space Complexity : O(1)
