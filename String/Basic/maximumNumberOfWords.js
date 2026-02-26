5b468f922f83ca10800cc0b6657e85f9d26a4bb9
Gunjan Singh <gunjansingh72002@gmail.com>
2026-02-26 09:24:42 +0530

    commit

--- String/Basic/maximumNumberOfWords.js
+++ String/Basic/maximumNumberOfWords.js
@@ -2,5 +2,24 @@
 
 
 function mostWordsFound(sentences){
+    let max = 0
 
+    for(let i = 0; i < sentences.length; i++)
+    {
+        let wordCount = sentences[i].split(" ").length
+        max = Math.max(max, wordCount)
+    }
+    return max
 }
+
+console.log(mostWordsFound([
+    "alice and bob love leetcode",
+    "i think so too",
+    "this is great thanks very much"                   //6
+]))
+
+console.log(mostWordsFound([
+    "please wait",
+    "continue to fight",
+    "continue to win"                                    //3
+]))                                                       //Time Complexity : O(N*M)
