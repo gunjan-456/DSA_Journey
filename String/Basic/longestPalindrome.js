126cd26b952120fb26606569d002eee0597ee346
Gunjan Singh <gunjansingh72002@gmail.com>
2026-02-28 00:51:25 +0530

    commit

--- String/Basic/longestPalindrome.js
+++ String/Basic/longestPalindrome.js
@@ -2,5 +2,33 @@
 
 
 function longestPalindrome(s){
+    let map = {}
+    let length = 0
+    let hasOdd = false
+
+    for(let char of s)
+    {
+        map[char] = (map[char] || 0) + 1
+    }
+
+    for(let key in map)
+    {
+        let count = map[key]
+
+        length += Math.floor(count / 2) * 2
+
+        if(count % 2 === 1)
+        {
+            hasOdd = true
+        }
+    }
+    if(hasOdd)
+    {
+        length += 1
+    }
+    return length
 
 }
+console.log(longestPalindrome("abccccdd"))            //7
+console.log(longestPalindrome("abcaa"))               //3                          Time Complexity : O(n)
+                                                                                 //Space Complexity : O(1)
