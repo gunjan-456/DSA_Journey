a0ff1ee1ed045abd85480f79ec0394734e3d7749
Gunjan Singh <gunjansingh72002@gmail.com>
2026-03-02 23:38:58 +0530

    commit

--- Array/Basic/nextGreaterElement_I.js
+++ Array/Basic/nextGreaterElement_I.js
@@ -1,6 +1,23 @@
 //Leetcode : 496
 
+function nextGreaterElement(nums1, nums2) {
+  let stack = []
+  let map = {}
 
-function nextGreaterElement(nums1, nums2){
-    
+  for (let num of nums2) {
+
+    while (stack.length && num > stack[stack.length - 1]) {
+      let smaller = stack.pop()
+      map[smaller] = num
+    }
+
+    stack.push(num)
+  }
+
+  while (stack.length) {
+    map[stack.pop()] = -1
+  }
+
+  return nums1.map(num => map[num])
 }
+console.log(nextGreaterElement([4,1,2], [1,3,4,2]))             //[ -1, 3, -1 ]         Time Complexity : O(n)
