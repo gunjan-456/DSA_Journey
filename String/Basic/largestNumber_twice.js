ca37759536ceec2521f59f3c8bcbe5098902f2b1
Gunjan Singh <gunjansingh72002@gmail.com>
2026-03-08 23:04:32 +0530

    commit

--- String/Basic/largestNumber_twice.js
+++ String/Basic/largestNumber_twice.js
@@ -1,5 +1,25 @@
 //Leetcode : 747
 
 function dominantIndex(nums){
+    let max = -1
+    let index = -1
+
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > max) {
+            max = nums[i]
+            index = i
+        }
+    }
+
+    for (let i = 0; i < nums.length; i++) {
+        if (i !== index && max < 2 * nums[i]) {
+            return -1
+        }
+    }
+
+    return index
+
     
 }
+
+console.log(dominantIndex([3, 6, 1, 0]))                //1
