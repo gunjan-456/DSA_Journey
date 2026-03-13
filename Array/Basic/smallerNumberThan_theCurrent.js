6c4c818eef25288559e0bd469d0d7fc3347087ed
Gunjan Singh <gunjansingh72002@gmail.com>
2026-03-13 23:57:33 +0530

    commit

---
+++ Array/Basic/smallerNumberThan_theCurrent.js
@@ -0,0 +1,23 @@
+//Leetcode : 1365
+
+
+function smallerNumbersThanCurrent(nums)
+{
+    let ans = []
+    for(let i = 0; i < nums.length; i++)
+    {
+        let count = 0
+        for(let j = 0; j < nums.length; j++)
+        {
+            if(nums[i] > nums[j])
+            {
+                count++
+            }
+        }
+        ans.push(count)
+    }
+    return ans
+}
+console.log(smallerNumbersThanCurrent([8,1,2,2,3]))
+console.log(smallerNumbersThanCurrent([6,5,4,8]))
+console.log(smallerNumbersThanCurrent([7,7,7,7]))
